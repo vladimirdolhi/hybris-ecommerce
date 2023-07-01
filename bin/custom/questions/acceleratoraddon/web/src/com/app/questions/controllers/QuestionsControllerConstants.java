b52023c65cc4b2966222c9b31bac3881c4cb1a9b
Vladimir Dolgiy <vladimirdolgijv@gmail.com>
2023-07-02 02:08:07 +0300

    create questions addon

---
+++ bin/custom/questions/acceleratoraddon/web/src/com/app/questions/controllers/QuestionsControllerConstants.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.app.questions.controllers;
+
+/**
+ */
+public interface QuestionsControllerConstants
+{
+	// implement here controller constants used by this extension
+}
